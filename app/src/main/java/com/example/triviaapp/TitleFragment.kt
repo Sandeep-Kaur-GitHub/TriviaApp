bd1df087d6617da6f2ec42ef29ac6289bd314cda
Sandeep-Kaur-GitHub <kaur94sandeep@gmail.com>
2022-07-25 13:28:29 +0530

    Add Navigation graph

--- app/src/main/java/com/example/triviaapp/TitleFragment.kt
+++ app/src/main/java/com/example/triviaapp/TitleFragment.kt
@@ -6,21 +6,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
 import com.example.triviaapp.databinding.FragmentTitleBinding
 
 
 class TitleFragment : Fragment() {
     private lateinit var binding: FragmentTitleBinding
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
       binding=DataBindingUtil.inflate(inflater,R.layout.fragment_title,container,false)
+        binding.playButton.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment))
         return binding.root
     }
 
